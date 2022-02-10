3aeaace88b59c5d6f2dec8fe28b549a858ad1ee3
Ilikemining1 <spbach1234@gmail.com>
2022-02-09 21:58:33 -0500

    Commented a ton of stuff
    Commented stuff for clarity and added .vscode to gitignore.

--- main/parallesp_main.c
+++ main/parallesp_main.c
@@ -17,27 +17,27 @@ QueueHandle_t ioQueue;
 QueueHandle_t parallelByteQueue;
 gptimer_handle_t handshakeTimer;
 
-static void IRAM_ATTR getParallelByteISR(void) {
-	REG_SET_BIT(GPIO_OUT1_W1TS_REG, BIT1);  // Set Busy High
+static void IRAM_ATTR getParallelByteISR(void) {  // ISR to grab byte from GPIO
+	REG_SET_BIT(GPIO_OUT1_W1TS_REG, BIT1);  // Set BUSY High
 	uint32_t ioPort = REG_READ(GPIO_IN_REG); // Read GPIO Register, all 32 pins
 	xQueueSendFromISR(ioQueue, &ioPort, NULL);  // Send GPIO data to register to be processed
-	REG_SET_BIT(GPIO_OUT1_W1TC_REG, BIT0); // Set ACK low
-	gptimer_start(handshakeTimer);
+	REG_SET_BIT(GPIO_OUT1_W1TC_REG, BIT0); // Set /ACK low
+	gptimer_start(handshakeTimer); // Start timer for other half of handshake
 }
 
 static void IRAM_ATTR completeHandshake(void) {
-	gptimer_stop(handshakeTimer);
+	gptimer_stop(handshakeTimer);  // Stop and reset one-shot timer
 	gptimer_set_raw_count(handshakeTimer, 0);
-	REG_SET_BIT(GPIO_OUT1_W1TS_REG, BIT0);  // Reset Busy and ACK
+	REG_SET_BIT(GPIO_OUT1_W1TS_REG, BIT0);  // Reset BUSY and /ACK
 	REG_SET_BIT(GPIO_OUT1_W1TC_REG, BIT1);
 }
 
 static void configureIO(void) { 
 	/* The ParallESP requires a minimum of 11 GPIOs.  9 inputs (Data Bits 0-7 and Strobe),
-	   and 2 outputs (ACK and Busy).  Additional data lines (Auto Selection, OOP Emulation, Error State)
+	   and 2 outputs (/ACK and BUSY).  Additional data lines (Auto Selection, OOP Emulation, Error State)
 	   and bidirectional parallel to be implemented at a later date.  The following function initializes
-	   IO for use.  GPIO Use is as follows: Strobe (4), D0-D7 (18, 19, 21, 22, 23, 25, 26, 27), ACK (32),
-	   and Busy (33). */
+	   IO for use.  GPIO Use is as follows: /STROBE (4), D0-D7 (18, 19, 21, 22, 23, 25, 26, 27), /ACK (32),
+	   and BUSY (33). */
 	
 	printf("Configuring interrupt:\n");
 	gpio_config_t intConfig = {
@@ -71,11 +71,11 @@ static void configureIO(void) {
 	};
 	gpio_config(&inConfig);
 
-	REG_SET_BIT(GPIO_OUT1_W1TS_REG, BIT0);
+	REG_SET_BIT(GPIO_OUT1_W1TS_REG, BIT0);  // Set initial state of flow control lines.  BUSY low, /ACK high.
 	REG_SET_BIT(GPIO_OUT1_W1TC_REG, BIT1);
 }
 
-static void configureTimer(void) {
+static void configureTimer(void) {  // Initialize One-Shot Timer Parameters
 	gptimer_config_t handshakeTimerConfig = {
 		.clk_src = GPTIMER_CLK_SRC_APB,
 		.direction = GPTIMER_COUNT_UP,
@@ -89,21 +89,22 @@ static void configureTimer(void) {
 	gptimer_event_callbacks_t handshakeCallback = {
 		.on_alarm = completeHandshake,
 	};
-	gptimer_register_event_callbacks(handshakeTimer, &handshakeCallback, NULL);
+	gptimer_register_event_callbacks(handshakeTimer, &handshakeCallback, NULL); // Set up timer callback
 }
 
-static void processIoToData(void) {
+static void processIoToData(void) {  // Task takes the uint32_t of the GPIO register and produces chars.  Pull from ioQueue, place on parallelDataQueue.
 	uint32_t ioData = 0;
 
 	while(1) {
-		xQueueReceive(ioQueue, &ioData, portMAX_DELAY);
-		char dataOut = ((ioData & 0xC0000) >> 18) + ((ioData & 0xE00000) >> 19) + ((ioData & 0xE000000) >> 20);
-		xQueueSend(parallelByteQueue, &dataOut, (TickType_t) 0);
+		xQueueReceive(ioQueue, &ioData, portMAX_DELAY);  // Block task if no data is available
+		char dataOut = ((ioData & 0xC0000) >> 18) + ((ioData & 0xE00000) >> 19) + ((ioData & 0xE000000) >> 20);  // Mask and shift to extract the 8 parallel bits.
+		xQueueSend(parallelByteQueue, &dataOut, portMAX_DELAY);
 	}
 }
 
-static void outputData(void) {
+static void outputData(void) { // If there is data to send, task will write it to the serial port.
 	char dataToSend = 0;
+
 	while(1) {
 		xQueueReceive(parallelByteQueue, &dataToSend, portMAX_DELAY);
 		printf("%c", dataToSend);
@@ -111,13 +112,13 @@ static void outputData(void) {
 }
 
 void app_main(void) {
-	printf("ParallESP Parallel Printer Emulator\n");
+	printf("ParallESP Parallel Printer Emulator\n"); // Do initialization stuffs.  Configure IO and Timer:
 	printf("Initializing...\n");
 	
 	configureIO();
 	configureTimer();
 
-	ioQueue = xQueueCreate(1000, sizeof(uint32_t));
+	ioQueue = xQueueCreate(1000, sizeof(uint32_t));  // Set up Queues.
 	parallelByteQueue = xQueueCreate(1000, sizeof(char));
 	
 	if ((ioQueue == NULL) || (parallelByteQueue == NULL)) {
@@ -126,6 +127,6 @@ void app_main(void) {
 		esp_restart();
 	}
 	
-	xTaskCreate(processIoToData, "Process IO Data to Chars", 4096, NULL, tskIDLE_PRIORITY, NULL);
+	xTaskCreate(processIoToData, "Process IO Data to Chars", 4096, NULL, tskIDLE_PRIORITY, NULL);  // Start data processing tasks.
 	xTaskCreate(outputData, "Output Data over Serial", 4096, NULL, tskIDLE_PRIORITY, NULL);
 }
